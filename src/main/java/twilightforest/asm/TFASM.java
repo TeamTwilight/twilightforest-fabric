c94700efc1e82b2497b4af58a1db65491a1ab9cb
alpha <alphamodeyt@gmail.com>
2022-05-08 12:00:01 -0500

    implement maprendercontext.js

--- src/main/java/twilightforest/asm/TFASM.java
+++ src/main/java/twilightforest/asm/TFASM.java
@@ -4,10 +4,7 @@ import com.chocohead.mm.api.ClassTinkerers;
 import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.MappingResolver;
 import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.InsnNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.VarInsnNode;
+import org.objectweb.asm.tree.*;
 
 public class TFASM implements Runnable {
     @Override
@@ -175,6 +172,126 @@ public class TFASM implements Runnable {
                                         "twilightforest/ASMHooks",
                                         "renderMutiparts",
                                         "(Ljava/lang/Iterable;)Ljava/lang/Iterable;",
+                                        false
+                                )
+                        )
+                );
+            });
+        });
+
+        // maprendercontext.js
+        // ItemInHandRenderer
+        String itemStackClass = resolver.mapClassName("intermediary", "net.minecraft.class_1799").replace('.', '/');
+        ClassTinkerers.addTransformation(resolver.mapClassName("intermediary", "net.minecraft.class_759"), classNode -> {
+            classNode.methods.forEach(methodNode -> {
+                if (!methodNode.name.equals(FabricLoader.getInstance().isDevelopmentEnvironment() ? "renderArmWithItem" : "method_3228"))
+                    return;
+                var /*org.objectweb.asm.tree.InsnList*/ instructions = methodNode.instructions;
+                var i = 0;
+                for (var index = instructions.size() - 1; index > 0; index--) {
+                    var /*org.objectweb.asm.tree.FieldInsnNode*/ node = instructions.get(index);
+                    if (i == 0 &&
+
+                            node instanceof FieldInsnNode fieldInsnNode &&
+
+                            node.getOpcode() == Opcodes.GETSTATIC &&
+
+                            equate(fieldInsnNode.owner, resolver.mapClassName("intermediary", "net.minecraft.class_1802").replace('.', '/')) &&
+
+                            equate(fieldInsnNode.name, FabricLoader.getInstance().isDevelopmentEnvironment() ? "FILLED_MAP" : "field_8204")
+
+                    )
+                        i = index + 1;
+
+                }
+                instructions.insert(
+                        instructions.get(i),
+                        ASM.listOf(
+                                new VarInsnNode(Opcodes.ALOAD, 6),
+                                new MethodInsnNode(
+                                        Opcodes.INVOKESTATIC,
+                                        "twilightforest/ASMHooks",
+                                        "shouldMapRender",
+                                        "(ZL" + itemStackClass + ";)Z",
+                                        false
+                                )
+                        )
+                );
+            });
+        });
+        String clientLevelName = resolver.mapClassName("intermediary", "net.minecraft.class_638").replace('.', '/');
+        String minecraftClass = resolver.mapClassName("intermediary", "net.minecraft.class_310").replace('.', '/');
+        String mapSaveDataClass = resolver.mapClassName("intermediary", "net.minecraft.class_22").replace('.', '/');
+        String levelClass = resolver.mapClassName("intermediary", "net.minecraft.class_1937").replace('.', '/');
+        ClassTinkerers.addTransformation(resolver.mapClassName("intermediary", "net.minecraft.class_759"), classNode -> {
+            classNode.methods.forEach(methodNode -> {
+                if (!methodNode.name.equals(FabricLoader.getInstance().isDevelopmentEnvironment() ? "renderMap" : "method_3223"))
+                    return;
+                var /*org.objectweb.asm.tree.InsnList*/ instructions = methodNode.instructions;
+                AbstractInsnNode insn = null;
+                for (var index = 0; index < instructions.size() - 1; index++) {
+                    var /*org.objectweb.asm.tree.VarInsnNode*/ node = instructions.get(index);
+                    if (insn == null &&
+
+                            node instanceof VarInsnNode varInsnNode &&
+
+                            node.getOpcode() == Opcodes.ASTORE &&
+
+                            varInsnNode.var == 6
+
+                    )
+                        insn = node;
+
+                }
+                instructions.insertBefore(
+                        insn,
+                        ASM.listOf(
+                                new VarInsnNode(Opcodes.ALOAD, 4),
+                                new VarInsnNode(Opcodes.ALOAD, 0),
+                                new FieldInsnNode(Opcodes.GETFIELD, "net/minecraft/client/renderer/ItemInHandRenderer", FabricLoader.getInstance().isDevelopmentEnvironment() ? "minecraft" : "field_4050", "L" + minecraftClass +";"),
+                                new FieldInsnNode(Opcodes.GETFIELD, minecraftClass, FabricLoader.getInstance().isDevelopmentEnvironment() ? "level" : "field_1687", "L" + clientLevelName + ";"),
+                                new MethodInsnNode(
+                                        Opcodes.INVOKESTATIC,
+                                        "twilightforest/ASMHooks",
+                                        "renderMapData",
+                                        "(L" + mapSaveDataClass + ";L" + itemStackClass + ";L" + levelClass + ";)L" + mapSaveDataClass + ";",
+                                        false
+                                )
+                        )
+                );
+            });
+        });
+        // MapItem
+        ClassTinkerers.addTransformation(resolver.mapClassName("intermediary", "net.minecraft.class_1806"), classNode -> {
+            classNode.methods.forEach(methodNode -> {
+                if (!methodNode.name.equals(FabricLoader.getInstance().isDevelopmentEnvironment() ? "appendHoverText" : "method_9568"))
+                    return;
+                var /*org.objectweb.asm.tree.InsnList*/ instructions = methodNode.instructions;
+                AbstractInsnNode insn = null;
+                for (var index = 0; index < instructions.size() - 1; index++) {
+                    var /*org.objectweb.asm.tree.VarInsnNode*/ node = instructions.get(index);
+                    if (insn == null &&
+
+                            node instanceof VarInsnNode varInsnNode &&
+
+                            node.getOpcode() == Opcodes.ASTORE &&
+
+                            varInsnNode.var == 6
+
+                    )
+                        insn = node;
+
+                }
+                instructions.insertBefore(
+                        insn,
+                        ASM.listOf(
+                                new VarInsnNode(Opcodes.ALOAD, 1),
+                                new VarInsnNode(Opcodes.ALOAD, 2),
+                                new MethodInsnNode(
+                                        Opcodes.INVOKESTATIC,
+                                        "twilightforest/ASMHooks",
+                                        "renderMapData",
+                                        "(L" + mapSaveDataClass +";L" + itemStackClass + ";L" + levelClass + ";)L" + mapSaveDataClass + ";",
                                         false
                                 )
                         )
---
+++ src/main/java/twilightforest/mixin/client/MapRendererMapInstanceMixin.java
@@ -0,0 +1,8 @@
+package twilightforest.mixin.client;
+
+import net.minecraft.client.gui.MapRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+
+@Mixin(MapRenderer.MapInstance.class)
+public class MapRendererMapInstanceMixin {
+}
