9386a1baa83059e36d0a67c7e19c7491fdd96732
TropheusJ <jverrellijr@icloud.com>
2022-08-05 23:53:17 -0400

    render layers

--- src/main/java/twilightforest/client/RenderLayerRegistration.java
+++ src/main/java/twilightforest/client/RenderLayerRegistration.java
@@ -9,29 +9,36 @@ import twilightforest.init.TFBlocks;
 public class RenderLayerRegistration {
 	@SuppressWarnings("removal")
 	public static void init() {
-		RenderType cutout = RenderType.cutout();
-		RenderType translucent = RenderType.translucent();
-
-		//FIXME these blocks absolutely refuse to use the render types defined in the json
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.CANDELABRA.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.TWISTED_STONE_PILLAR.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.FIERY_BLOCK.get(), translucent);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.YELLOW_CASTLE_RUNE_BRICK.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.VIOLET_CASTLE_RUNE_BRICK.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.PINK_CASTLE_RUNE_BRICK.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BLUE_CASTLE_RUNE_BRICK.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.YELLOW_CASTLE_DOOR.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.VIOLET_CASTLE_DOOR.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.PINK_CASTLE_DOOR.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BLUE_CASTLE_DOOR.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.GREEN_THORNS.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BROWN_THORNS.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BURNT_THORNS.get(), cutout);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.PINK_FORCE_FIELD.get(), translucent);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BLUE_FORCE_FIELD.get(), translucent);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.GREEN_FORCE_FIELD.get(), translucent);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.VIOLET_FORCE_FIELD.get(), translucent);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.ORANGE_FORCE_FIELD.get(), translucent);
-		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.RED_THREAD.get(), cutout);
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BEANSTALK_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BLUE_CASTLE_DOOR.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BLUE_CASTLE_RUNE_BRICK.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BLUE_FORCE_FIELD.get(), RenderType.translucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BROWN_THORNS.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.BURNT_THORNS.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.CANDELABRA.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.CANOPY_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.DARK_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.FIERY_BLOCK.get(), RenderType.translucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.GREEN_FORCE_FIELD.get(), RenderType.translucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.GREEN_THORNS.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.MANGROVE_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.MINING_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.ORANGE_FORCE_FIELD.get(), RenderType.translucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.PINK_CASTLE_DOOR.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.PINK_CASTLE_RUNE_BRICK.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.PINK_FORCE_FIELD.get(), RenderType.translucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.RAINBOW_OAK_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.RED_THREAD.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.SORTING_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.THORN_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.TIME_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.TRANSFORMATION_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.TWILIGHT_OAK_LEAVES.get(), RenderType.cutoutMipped());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.TWISTED_STONE_PILLAR.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.VIOLET_CASTLE_DOOR.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.VIOLET_CASTLE_RUNE_BRICK.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.VIOLET_FORCE_FIELD.get(), RenderType.translucent());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.YELLOW_CASTLE_DOOR.get(), RenderType.cutout());
+		BlockRenderLayerMap.INSTANCE.putBlock(TFBlocks.YELLOW_CASTLE_RUNE_BRICK.get(), RenderType.cutout());
 	}
 }
