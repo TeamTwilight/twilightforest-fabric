037f5bbcfd5d3415f7eb922c56f63d28af45850e
TropheusJ <jverrellijr@icloud.com>
2022-12-17 23:34:22 -0500

    fix sortingwood core

--- src/main/java/twilightforest/block/SortLogCoreBlock.java
+++ src/main/java/twilightforest/block/SortLogCoreBlock.java
@@ -1,6 +1,7 @@
 package twilightforest.block;
 
 import io.github.fabricators_of_create.porting_lib.transfer.TransferUtil;
+import net.fabricmc.fabric.api.transfer.v1.item.InventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.item.ItemVariant;
 import net.fabricmc.fabric.api.transfer.v1.item.PlayerInventoryStorage;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
@@ -12,9 +13,8 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.entity.vehicle.ContainerEntity;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.phys.AABB;
 import net.minecraft.world.phys.Vec3;
 import twilightforest.TFConfig;
@@ -49,17 +49,25 @@ public class SortLogCoreBlock extends SpecialMagicLogBlock {
 				Storage<ItemVariant> storage = TransferUtil.getItemStorage(level, blockPos);
 
 				if (storage != null) {
-					if (Math.abs(blockPos.getX() - pos.getX()) <= 2 && Math.abs(blockPos.getY() - pos.getY()) <= 2 && Math.abs(blockPos.getZ() - pos.getZ()) <= 2) {
+					boolean inInputRange = Math.abs(blockPos.getX() - pos.getX()) <= 2
+							&& Math.abs(blockPos.getY() - pos.getY()) <= 2
+							&& Math.abs(blockPos.getZ() - pos.getZ()) <= 2;
+					if (inInputRange && storage.supportsExtraction()) {
 						inputHandlers.put(storage, Vec3.upFromBottomCenterOf(blockPos, 1.9D));
-					} else outputHandlers.put(storage, Vec3.upFromBottomCenterOf(blockPos, 1.9D));
+					} else if (!inInputRange && storage.supportsInsertion()) {
+						outputHandlers.put(storage, Vec3.upFromBottomCenterOf(blockPos, 1.9D));
+					}
 				}
 			}
 		}
 
-		// TODO: PORT other entities
+		// fabric: transfer API entity support when
 		level.getEntities((Entity) null, new AABB(pos).inflate(2), entity -> entity.isAlive() && entity.getType().is(EntityTagGenerator.SORTABLE_ENTITIES)).forEach(entity -> {
 			if (entity instanceof Player player)
 				inputHandlers.put(PlayerInventoryStorage.of(player), entity.position().add(0D, entity.getBbHeight() + 0.9D, 0D));
+			else if (entity instanceof ContainerEntity container) {
+				inputHandlers.put(InventoryStorage.of(container, null), entity.position().add(0D, entity.getBbHeight() + 0.9D, 0D));
+			}
 		});
 
 		if (inputHandlers.isEmpty()) return;
@@ -69,6 +77,10 @@ public class SortLogCoreBlock extends SpecialMagicLogBlock {
 				Storage<ItemVariant> playerInv = PlayerInventoryStorage.of(player);
 				if (!inputHandlers.containsKey(playerInv))
 					outputHandlers.put(playerInv, entity.position().add(0D, entity.getBbHeight() + 0.9D, 0D));
+			} else if (entity instanceof ContainerEntity container) {
+				InventoryStorage storage = InventoryStorage.of(container, null);
+				if (!inputHandlers.containsKey(storage))
+					outputHandlers.put(storage, entity.position().add(0D, entity.getBbHeight() + 0.9D, 0D));
 			}
 		});
 
@@ -77,60 +89,50 @@ public class SortLogCoreBlock extends SpecialMagicLogBlock {
 		for (Storage<ItemVariant> inputIItemHandler : inputHandlers.keySet()) {
 			try (Transaction t = TransferUtil.getTransaction()) {
 				for (StorageView<ItemVariant> slot : inputIItemHandler) {
-					long input = TransferUtil.simulateExtractView(slot, slot.getResource(), 1);
-					if (input != 0) {
-						boolean transferred = false;
+					if (slot.isResourceBlank())
+						continue;
+					ItemVariant contents = slot.getResource();
+					boolean transferred = false;
 
-						Map<Integer, Storage<ItemVariant>> outputsByCount = new HashMap<>();
+					Map<Long, Storage<ItemVariant>> outputsByCount = new HashMap<>();
 
-						for (Storage<ItemVariant> outputIItemHandler : outputHandlers.keySet()) {
-							int count = 0;
-							for (StorageView<ItemVariant> slotJ : inputIItemHandler) {
-								if (slotJ.getResource().isOf(slot.getResource().getItem())) count += slotJ.getAmount();
-							}
-							if (count > 0) outputsByCount.put(count, outputIItemHandler);
-						}
-
-						for (Integer count : outputsByCount.keySet().stream().sorted(Comparator.comparingInt(Integer::intValue).reversed()).toList()) {
-							Storage<ItemVariant> outputIItemHandler = outputsByCount.get(count);
-							ItemVariant firstProperStack = ItemVariant.blank();
-							for (StorageView<ItemVariant> slotJ : inputIItemHandler) {
-								ItemStack outputStack = slotJ.getResource().toStack((int) slotJ.getAmount());
-
-								if (firstProperStack.isBlank() && outputStack.isEmpty()) {
-									firstProperStack = slotJ.getResource(); //We reference the index of the first empty slot, in case there is no stacks that aren't at max size
-								} else if (ItemStack.isSameItemSameTags(slot.getResource().toStack((int) input), outputStack)
-										&& outputStack.getCount() < outputStack.getMaxStackSize()
-										/*&& outputStack.getCount() < outputIItemHandler.getSlotLimit(j)*/) {
-									firstProperStack = slotJ.getResource();
-									break;
-								}
-							}
-							if (firstProperStack != null) { //If there weren't any non-full stacks, we transfer to an empty space instead
-								long newStack = slot.extract(slot.getResource(), 1, t);
-								if (newStack != 0 && outputIItemHandler.simulateInsert(firstProperStack, newStack, t) == 0) {
-									outputIItemHandler.insert(firstProperStack, newStack, t);
-									transferred = true;
-
-									Vec3 xyz = outputHandlers.get(outputIItemHandler);
-									Vec3 diff = inputHandlers.get(inputIItemHandler).subtract(xyz);
-
-									for (ServerPlayer serverplayer : ((ServerLevel) level).players()) {//This is just particle math, we send a particle packet to every player in range
-										if (serverplayer.distanceToSqr(xyz) < 4096.0D) {
-											ParticlePacket particlePacket = new ParticlePacket();
-											double x = diff.x - 0.25D + rand.nextDouble() * 0.5D;
-											double y = diff.y - 1.75D + rand.nextDouble() * 0.5D;
-											double z = diff.z - 0.25D + rand.nextDouble() * 0.5D;
-											particlePacket.queueParticle(TFParticleType.SORTING_PARTICLE.get(), false, xyz, new Vec3(x, y, z).scale(1D / diff.length()));
-											TFPacketHandler.CHANNEL.sendToClient(particlePacket, serverplayer);
-										}
-									}
-									break;
-								}
-							}
-						}
-						if (transferred) break;
+					for (Storage<ItemVariant> outputIItemHandler : outputHandlers.keySet()) {
+						long count = outputIItemHandler.simulateExtract(contents, Long.MAX_VALUE, t);
+						if (count > 0) outputsByCount.put(count, outputIItemHandler);
 					}
+
+					for (Long count : outputsByCount.keySet().stream().sorted(Comparator.comparingLong(Long::longValue).reversed()).toList()) {
+						Storage<ItemVariant> outputIItemHandler = outputsByCount.get(count);
+
+						try (Transaction nested = t.openNested()) {
+							long extracted = slot.extract(contents, 1, nested);
+							if (extracted != 1)
+								break; // nothing to extract, this shouldn't happen really
+							long inserted = outputIItemHandler.insert(contents, 1, nested);
+							if (inserted != 1)
+								continue; // didn't insert the right amount, skip it
+
+							// everything is good
+							transferred = true;
+							nested.commit();
+
+							Vec3 xyz = outputHandlers.get(outputIItemHandler);
+							Vec3 diff = inputHandlers.get(inputIItemHandler).subtract(xyz);
+
+							for (ServerPlayer serverplayer : ((ServerLevel) level).players()) {//This is just particle math, we send a particle packet to every player in range
+								if (serverplayer.distanceToSqr(xyz) < 4096.0D) {
+									ParticlePacket particlePacket = new ParticlePacket();
+									double x = diff.x - 0.25D + rand.nextDouble() * 0.5D;
+									double y = diff.y - 1.75D + rand.nextDouble() * 0.5D;
+									double z = diff.z - 0.25D + rand.nextDouble() * 0.5D;
+									particlePacket.queueParticle(TFParticleType.SORTING_PARTICLE.get(), false, xyz, new Vec3(x, y, z).scale(1D / diff.length()));
+									TFPacketHandler.CHANNEL.sendToClient(particlePacket, serverplayer);
+								}
+							}
+							break;
+						}
+					}
+					if (transferred) break;
 				}
 				t.commit();
 			}
