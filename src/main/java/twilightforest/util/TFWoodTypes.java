8118e38a0854cfc89475c8997565f94b2e5c1023
AlphaMode <alphamodeyt@gmail.com>
2023-04-01 20:06:07 -0500

    Fix actions failing

--- src/main/java/twilightforest/util/TFWoodTypes.java
+++ src/main/java/twilightforest/util/TFWoodTypes.java
@@ -1,5 +1,6 @@
 package twilightforest.util;
 
+import net.fabricmc.fabric.api.object.builder.v1.block.type.WoodTypeRegistry;
 import net.minecraft.world.level.block.state.properties.BlockSetType;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import twilightforest.TwilightForestMod;
@@ -15,12 +16,12 @@ public class TFWoodTypes {
 	public static final BlockSetType MINING_WOOD_SET = new BlockSetType(TwilightForestMod.prefix("mining").toString());
 	public static final BlockSetType SORTING_WOOD_SET = new BlockSetType(TwilightForestMod.prefix("sorting").toString());
 
-	public static final WoodType TWILIGHT_OAK_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("twilight_oak").toString(), TWILIGHT_OAK_SET));
-	public static final WoodType CANOPY_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("canopy").toString(), CANOPY_WOOD_SET));
-	public static final WoodType MANGROVE_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("mangrove").toString(), MANGROVE_WOOD_SET));
-	public static final WoodType DARKWOOD_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("darkwood").toString(), DARKWOOD_WOOD_SET));
-	public static final WoodType TIMEWOOD_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("timewood").toString(), TIMEWOOD_WOOD_SET));
-	public static final WoodType TRANSFORMATION_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("transformation").toString(), TRANSFORMATION_WOOD_SET));
-	public static final WoodType MINING_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("mining").toString(), MINING_WOOD_SET));
-	public static final WoodType SORTING_WOOD_TYPE = WoodType.register(new WoodType(TwilightForestMod.prefix("sorting").toString(), SORTING_WOOD_SET));
+	public static final WoodType TWILIGHT_OAK_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("twilight_oak"), TWILIGHT_OAK_SET);
+	public static final WoodType CANOPY_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("canopy"), CANOPY_WOOD_SET);
+	public static final WoodType MANGROVE_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("mangrove"), MANGROVE_WOOD_SET);
+	public static final WoodType DARKWOOD_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("darkwood"), DARKWOOD_WOOD_SET);
+	public static final WoodType TIMEWOOD_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("timewood"), TIMEWOOD_WOOD_SET);
+	public static final WoodType TRANSFORMATION_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("transformation"), TRANSFORMATION_WOOD_SET);
+	public static final WoodType MINING_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("mining"), MINING_WOOD_SET);
+	public static final WoodType SORTING_WOOD_TYPE = WoodTypeRegistry.register(TwilightForestMod.prefix("sorting"), SORTING_WOOD_SET);
 }
