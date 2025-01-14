c676471fd5e3b2f39fe159b8513fcd4a1e8904aa
Tamaized <9671313+Tamaized@users.noreply.github.com>
2025-01-14 16:33:43 -0500

    TomeSpawnerBlockEntity parity comment

--- src/main/java/twilightforest/block/entity/TomeSpawnerBlockEntity.java
+++ src/main/java/twilightforest/block/entity/TomeSpawnerBlockEntity.java
@@ -51,7 +51,7 @@ public class TomeSpawnerBlockEntity extends BlockEntity {
 			}
 
 			if (te.tomesLeft <= 0) {
-//				te.invalidateCaps();
+				// te.invalidateCaps(); [PARITY] No caps in fabric land
 				level.setBlockAndUpdate(pos, TFBlocks.EMPTY_CANOPY_BOOKSHELF.get().defaultBlockState());
 			} else {
 				level.setBlockAndUpdate(pos, state.setValue(TomeSpawnerBlock.BOOK_STAGES, te.tomesLeft));
