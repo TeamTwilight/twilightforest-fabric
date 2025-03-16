5fdb2cfe60a08dca788e8750b323965a94d2b0d9
drullkus <drullkus@gmail.com>
2025-03-16 15:55:03 -0700

    Another two for the modpackers

--- src/main/java/twilightforest/data/custom/StructureTemplateDefinitionGenerator.java
+++ src/main/java/twilightforest/data/custom/StructureTemplateDefinitionGenerator.java
@@ -4,7 +4,9 @@ import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 import twilightforest.TwilightForestMod;
+import twilightforest.world.components.structures.finalcastle.FinalCastleBellTower21Component;
 import twilightforest.world.components.structures.finalcastle.FinalCastleBossGazeboComponent;
+import twilightforest.world.components.structures.finalcastle.FinalCastleLargeTowerComponent;
 import twilightforest.world.components.structures.lichtowerrevamp.LichTowerPieces;
 
 import java.util.concurrent.CompletableFuture;
@@ -360,6 +362,8 @@ public class StructureTemplateDefinitionGenerator extends StructureTemplateDefin
 			"lich_tower/9x9/keepsake_casket"
 		);
 
+		this.add("final_castle/temp/large_tower", FinalCastleLargeTowerComponent.LARGE_TOWER_TEMP_POOL, 1);
+		this.add("final_castle/temp/bell_tower", FinalCastleBellTower21Component.BELL_TOWER_TEMP_POOL, 1);
 		this.add("final_castle/temp/gazebo", FinalCastleBossGazeboComponent.GAZEBO_TEMP_POOL, 1);
 	}
 }
