b66a59d2a61317b07cee8abd78971f3fe91c0bc7
Sleepy-Horse <andrei.kunitsin@gmail.com>
2025-03-18 23:42:14 +0100

    Optimize LazyArea for multithreading

--- src/main/java/twilightforest/world/components/layer/vanillalegacy/area/LazyArea.java
+++ src/main/java/twilightforest/world/components/layer/vanillalegacy/area/LazyArea.java
@@ -1,42 +1,46 @@
 package twilightforest.world.components.layer.vanillalegacy.area;
 
-import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.biome.Biome;
 import net.minecraft.world.level.biome.Biomes;
 import twilightforest.world.components.layer.vanillalegacy.Area;
 
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class LazyArea implements Area {
 	private final Area transformer;
-	private final Long2ObjectLinkedOpenHashMap<ResourceKey<Biome>> cachedSamples;
+	private final ConcurrentHashMap<Long, ResourceKey<Biome>> cachedSamples;
+	private final LinkedBlockingQueue<Long> evictionQueue;
 	private final int maxCache;
 
-	public LazyArea(Long2ObjectLinkedOpenHashMap<ResourceKey<Biome>> cache, int maxCache, Area transformer) {
+	public LazyArea(ConcurrentHashMap<Long, ResourceKey<Biome>> cache, LinkedBlockingQueue<Long> evictionQueue, int maxCache, Area transformer) {
 		this.cachedSamples = cache;
+		this.evictionQueue = evictionQueue;
 		this.maxCache = maxCache;
 		this.transformer = transformer;
 	}
 
 	@Override
 	public ResourceKey<Biome> getBiome(int biomeX, int biomeZ) {
-		long i = ChunkPos.asLong(biomeX, biomeZ);
-		synchronized (this.cachedSamples) {
-			ResourceKey<Biome> j = this.cachedSamples.get(i);
-			if (j != null && j != Biomes.THE_VOID) {
-				return j;
-			} else {
-				ResourceKey<Biome> k = this.transformer.getBiome(biomeX, biomeZ);
-				this.cachedSamples.put(i, k);
-				if (this.cachedSamples.size() > this.maxCache) {
-					for (int l = 0; l < this.maxCache / 16; ++l) {
-						this.cachedSamples.removeFirst();
-					}
-				}
-
-				return k;
+		long key = ChunkPos.asLong(biomeX, biomeZ);
+		ResourceKey<Biome> cached = cachedSamples.get(key);
+		if (cached != null && cached != Biomes.THE_VOID)
+			return cached;
+		ResourceKey<Biome> computed = transformer.getBiome(biomeX, biomeZ);
+		ResourceKey<Biome> existing = cachedSamples.putIfAbsent(key, computed);
+		if (existing != null && existing != Biomes.THE_VOID)
+			return existing;
+		evictionQueue.offer(key);
+		if (evictionQueue.size() > maxCache) {
+			for (int i = 0, limit = maxCache / 16; i < limit; i++) {
+				Long oldest = evictionQueue.poll();
+				if (oldest != null)
+					cachedSamples.remove(oldest);
 			}
 		}
+		return computed;
 	}
 
 	public int getMaxCache() {
