5abf954825436084d19172f5c7fa256208a29a5a
Sleepy-Horse <andrei.kunitsin@gmail.com>
2025-11-24 13:55:08 +0100

    Change default keybind for ITEM_DISPLAY_MAP_CYCLE_KEY from B to C

--- src/main/java/twilightforest/init/TFKeyBinds.java
+++ src/main/java/twilightforest/init/TFKeyBinds.java
@@ -12,7 +12,7 @@ public abstract class TFKeyBinds {
 	public static final Set<KeyMapping> KEY_MAPPINGS = new HashSet<>();
 
 	public static final KeyMapping RED_THREAD_VISION_KEY = register("red_thread_vision", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_UNKNOWN, TFKeyBindsCategories.TRAVELLERS_GEAR);
-	public static final KeyMapping ITEM_DISPLAY_MAP_CYCLE_KEY = register("item_display_map_cycle", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_B, TFKeyBindsCategories.TRAVELLERS_GEAR);
+	public static final KeyMapping ITEM_DISPLAY_MAP_CYCLE_KEY = register("item_display_map_cycle", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_C, TFKeyBindsCategories.TRAVELLERS_GEAR);
 	public static final KeyMapping ZOOM_KEY = register("zoom", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_Z, TFKeyBindsCategories.TRAVELLERS_GEAR);
 	public static final KeyMapping SWAP_HOTBAR_KEY = register("swap_hotbar", InputConstants.Type.KEYSYM, GLFW.GLFW_KEY_V, TFKeyBindsCategories.TRAVELLERS_GEAR);
 
