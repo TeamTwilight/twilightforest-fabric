cde2169844620e8c68295bd20692a1bf72dcbc53
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2025-12-03 09:00:30 +0100

    Use ASMUtil.findFieldInstructions

--- tf-asm/src/main/java/twilightforest/asm/transformers/entity/ResetStuckUnrestrainedTransformer.java
+++ tf-asm/src/main/java/twilightforest/asm/transformers/entity/ResetStuckUnrestrainedTransformer.java
@@ -4,9 +4,11 @@ import cpw.mods.modlauncher.api.ITransformer;
 import cpw.mods.modlauncher.api.ITransformerVotingContext;
 import cpw.mods.modlauncher.api.TargetType;
 import cpw.mods.modlauncher.api.TransformerVoteResult;
+import net.neoforged.coremod.api.ASMAPI;
 import org.jetbrains.annotations.NotNull;
 import org.objectweb.asm.Opcodes;
 import org.objectweb.asm.tree.*;
+import twilightforest.asm.ASMUtil;
 
 import java.util.Set;
 
@@ -17,25 +19,22 @@ public class ResetStuckUnrestrainedTransformer implements ITransformer<MethodNod
 
 	@Override
 	public @NotNull MethodNode transform(MethodNode node, ITransformerVotingContext ctx) {
-		InsnList insns = node.instructions;
-		for (AbstractInsnNode p = insns.getFirst(); p != null; p = p.getNext()) {
-			if (p.getOpcode() != Opcodes.GETFIELD) continue;
-			FieldInsnNode f = (FieldInsnNode) p;
-			if ("net/minecraft/world/entity/Entity".equals(f.owner)
-				&& "stuckSpeedMultiplier".equals(f.name)
-				&& "Lnet/minecraft/world/phys/Vec3;".equals(f.desc)) {
-				InsnList hook = new InsnList();
-				hook.add(new MethodInsnNode(
+		ASMUtil.findFieldInstructions(
+				node,
+				Opcodes.GETFIELD,
+				"net/minecraft/world/entity/Entity",
+				"stuckSpeedMultiplier"
+			)
+			.filter(f -> "Lnet/minecraft/world/phys/Vec3;".equals(f.desc))
+			.forEach(target -> node.instructions.insertBefore(target, ASMAPI.listOf(
+				new MethodInsnNode(
 					Opcodes.INVOKESTATIC,
 					"twilightforest/asmhooks/EntityHooks",
 					"resetStuckUnrestrained",
 					"(Lnet/minecraft/world/entity/Entity;)V"
-				));
-				hook.add(new VarInsnNode(Opcodes.ALOAD, 0));
-
-				insns.insertBefore(f, hook);
-			}
-		}
+				),
+				new VarInsnNode(Opcodes.ALOAD, 0)
+			)));
 		return node;
 	}
 
