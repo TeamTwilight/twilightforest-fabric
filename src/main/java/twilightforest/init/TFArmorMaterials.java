42770695c52369196a7bd981c9a18374d6ea15bc
Sleepy-Horse <andrei.kunitsin@gmail.com>
2025-12-13 12:12:45 +0100

    Nerf defense for chestplate

--- src/main/java/twilightforest/init/TFArmorMaterials.java
+++ src/main/java/twilightforest/init/TFArmorMaterials.java
@@ -5,7 +5,6 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.item.crafting.Ingredient;
 import net.neoforged.neoforge.registries.DeferredHolder;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -45,7 +44,7 @@ public class TFArmorMaterials {
 	public static final DeferredHolder<ArmorMaterial, ArmorMaterial> TRAVELLERS = ARMOR_MATERIALS.register("travellers", () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
 		map.put(ArmorItem.Type.BOOTS, 2);
 		map.put(ArmorItem.Type.LEGGINGS, 3);
-		map.put(ArmorItem.Type.CHESTPLATE, 5);
+		map.put(ArmorItem.Type.CHESTPLATE, 4);
 		map.put(ArmorItem.Type.HELMET, 2);
 		map.put(ArmorItem.Type.BODY, 4);
 	}), 0, SoundEvents.ARMOR_EQUIP_GENERIC, () -> Ingredient.of(TFItems.TANNED_LEATHER), List.of(new ArmorMaterial.Layer(TwilightForestMod.prefix("travellers"))), 0F, 0.0F));
