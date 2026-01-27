1a6a8c6ec1d1d49d6233cd507c42a555cdc66ef6
Sleepy-Horse <andrei.kunitsin@gmail.com>
2026-01-27 01:10:51 +0100

    Allow camps in mushroom_forest, forest, firefly_forest; run datagen

--- src/main/java/twilightforest/data/tags/BiomeTagGenerator.java
+++ src/main/java/twilightforest/data/tags/BiomeTagGenerator.java
@@ -61,7 +61,7 @@ public class BiomeTagGenerator extends BiomeTagsProvider {
 		this.tag(VALID_MUSHROOM_TOWER_BIOMES).add(TFBiomes.DENSE_MUSHROOM_FOREST);
 
 		this.tag(VALID_CAMP_BIOMES).add(
-			TFBiomes.OAK_SAVANNAH, TFBiomes.CLEARING
+			TFBiomes.OAK_SAVANNAH, TFBiomes.CLEARING, TFBiomes.MUSHROOM_FOREST, TFBiomes.FOREST, TFBiomes.FIREFLY_FOREST
 		);
 
 		this.tag(VALID_HOLLOW_TREE_BIOMES).add(
