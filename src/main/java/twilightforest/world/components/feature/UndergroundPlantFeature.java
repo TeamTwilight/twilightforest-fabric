a9683b9dd84d9ae3490ca8e9556212fa54c7b2d7
Fywchis <168770132+Fywchis@users.noreply.github.com>
2026-02-28 12:02:58 +0300

    fix oreberries spawning inside structure (#2532)
    * Add structure check
    
    * fixup! Delete unused import
    
    * Add new feature for underground_plants_in_str
    Add overload to UndergroundPlantFeature
    Fix logic in placing
    
    * Fix placing logic
    Fix troll vines not spawning
    
    * Change field name; inline if check; add sbb check
    
    ---------
    
    Co-authored-by: Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>

--- src/main/java/twilightforest/world/components/feature/UndergroundPlantFeature.java
+++ src/main/java/twilightforest/world/components/feature/UndergroundPlantFeature.java
@@ -2,6 +2,7 @@ package twilightforest.world.components.feature;
 
 import com.mojang.serialization.Codec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.SectionPos;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Block;
@@ -9,19 +10,40 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.BlockStateConfiguration;
+import net.minecraft.world.level.levelgen.structure.StructureStart;
 import twilightforest.init.TFBlocks;
+import twilightforest.util.landmarks.LandmarkUtil;
+
+import java.util.Optional;
 
 public class UndergroundPlantFeature extends Feature<BlockStateConfiguration> {
 	int maxCount;
+	boolean spawnInStructure;
 
 	public UndergroundPlantFeature(Codec<BlockStateConfiguration> config, int maxCount) {
 		super(config);
 		this.maxCount = maxCount;
+		this.spawnInStructure = false;
+
 	}
 
 	public UndergroundPlantFeature(Codec<BlockStateConfiguration> config) {
 		super(config);
 		this.maxCount = Integer.MAX_VALUE;
+		this.spawnInStructure = false;
+
+	}
+
+	public UndergroundPlantFeature(Codec<BlockStateConfiguration> config, int maxCount, boolean spawnInStructure) {
+		super(config);
+		this.maxCount = maxCount;
+		this.spawnInStructure = spawnInStructure;
+	}
+
+	public UndergroundPlantFeature(Codec<BlockStateConfiguration> config, boolean spawnInStructure) {
+		super(config);
+		this.maxCount = Integer.MAX_VALUE;
+		this.spawnInStructure = spawnInStructure;
 	}
 
 	@Override
@@ -46,9 +68,10 @@ public class UndergroundPlantFeature extends Feature<BlockStateConfiguration> {
 			}
 
 			BlockState state = ctx.config().state;
+			Optional<StructureStart> structureStart = LandmarkUtil.locateNearestLandmarkStart(world, SectionPos.blockToSectionCoord(pos.getX()), SectionPos.blockToSectionCoord(pos.getZ()));
 			if (state.is(TFBlocks.TROLLVIDR) && random.nextInt(10) == 0)
 				state = TFBlocks.UNRIPE_TROLLBER.get().defaultBlockState();
-			if (state.canSurvive(world, pos)) {
+			if (state.canSurvive(world, pos) && (spawnInStructure || structureStart.isEmpty() || !structureStart.get().getBoundingBox().isInside(pos))) {
 				world.setBlock(pos, state, Block.UPDATE_KNOWN_SHAPE | Block.UPDATE_CLIENTS);
 				placed++;
 			}
