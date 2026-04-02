b16dd6fda6e355a26c483bf9ad021d3818cff24f
Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
2026-04-02 16:35:33 +0200

    Revert "Make TFTeleportCommand teleport the player based to the heightmap instead of y conversion."
    This reverts commit f83423e4

--- src/main/java/twilightforest/command/TFTeleportCommand.java
+++ src/main/java/twilightforest/command/TFTeleportCommand.java
@@ -8,11 +8,10 @@ import com.mojang.brigadier.exceptions.SimpleCommandExceptionType;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.SectionPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.world.level.levelgen.Heightmap;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
 import twilightforest.init.TFDimension;
 
@@ -41,16 +40,16 @@ public class TFTeleportCommand {
 			throw DIMENSION_MISSING.create();  // should never happen
 		}
 
-		Vec3 pos = player.position();
-		twilight.getChunk(player.blockPosition());
-		double y = twilight.getHeight(Heightmap.Types.MOTION_BLOCKING, player.blockPosition().getX(), player.blockPosition().getZ());
-		Vec3 teleportPos = new Vec3(pos.x(), y, pos.z());
+		Vec3 pos = source.getPosition();
+		Level level = player.level();
+		double yConverted = (pos.y - level.getMinBuildHeight()) / (level.getMaxBuildHeight() - level.getMinBuildHeight()) * (twilight.getMaxBuildHeight() - twilight.getMinBuildHeight()) + twilight.getMinBuildHeight();
+		Vec3 teleportPos = new Vec3(pos.x, yConverted, pos.z);
 		if (!twilight.isInWorldBounds(BlockPos.containing(teleportPos)))
 			throw INVALID_POSITION.create();
 		player.teleportTo(twilight, teleportPos.x(), teleportPos.y(), teleportPos.z(), player.getYRot(), player.getXRot());
-		String formattedX = String.format(Locale.ROOT, "%.1f", pos.x());
-		String formattedY = String.format(Locale.ROOT, "%.1f", y);
-		String formattedZ = String.format(Locale.ROOT, "%.1f", pos.z());
+		String formattedX = String.format(Locale.ROOT, "%.1f", teleportPos.x());
+		String formattedY = String.format(Locale.ROOT, "%.1f", teleportPos.y());
+		String formattedZ = String.format(Locale.ROOT, "%.1f", teleportPos.z());
 		source.sendSuccess(() -> Component.translatable("commands.tffeature.teleport.success", formattedX, formattedY, formattedZ), false);
 		return Command.SINGLE_SUCCESS;
 	}
