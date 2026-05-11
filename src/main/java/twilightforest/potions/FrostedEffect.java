4cbdf02a1d4982fe8a668825aab7d039e3f1f5bf
a6tumn <shinigami7x@proton.me>
2026-05-11 10:48:34 -0400

    Port FrostedEffect (#2599)

--- src/main/java/twilightforest/potions/FrostedEffect.java
+++ src/main/java/twilightforest/potions/FrostedEffect.java
@@ -1,6 +1,7 @@
 package twilightforest.potions;
 
 import net.minecraft.resources.Identifier;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.entity.LivingEntity;
@@ -8,29 +9,26 @@ import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.ai.attributes.Attributes;
 import twilightforest.TwilightForestMod;
 
-import java.util.UUID;
-
 public class FrostedEffect extends MobEffect {
 	public static final Identifier MOVEMENT_SPEED_MODIFIER = TwilightForestMod.prefix("frosted_slowdown");
 	public static final double FROST_MULTIPLIER = -0.15D;
 
-	@SuppressWarnings("this-escape")
 	public FrostedEffect() {
 		super(MobEffectCategory.HARMFUL, 0x56CBFD);
 		this.addAttributeModifier(Attributes.MOVEMENT_SPEED, FrostedEffect.MOVEMENT_SPEED_MODIFIER, FROST_MULTIPLIER, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL);
 	}
 
 	@Override
-	public boolean applyEffectTick(LivingEntity living, int amplifier) {
-		living.setIsInPowderSnow(true);
-		if (amplifier > 0 && living.canFreeze()) {
-			living.setTicksFrozen(Math.min(living.getTicksRequiredToFreeze(), living.getTicksFrozen() + amplifier));
+	public boolean applyEffectTick(ServerLevel serverLevel, LivingEntity mob, int amplification) {
+		mob.setIsInPowderSnow(true);
+		if (amplification > 0 && mob.canFreeze()) {
+			mob.setTicksFrozen(Math.min(mob.getTicksRequiredToFreeze(), mob.getTicksFrozen() + amplification));
 		}
 		return true;
 	}
 
 	@Override
-	public boolean shouldApplyEffectTickThisTick(int duration, int amplifier) {
+	public boolean shouldApplyEffectTickThisTick(int tickCount, int amplification) {
 		return true;
 	}
 }
