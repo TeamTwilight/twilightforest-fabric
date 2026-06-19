9253cf35d69d203a8039f4d3fed2e25cde8a08bf
a6tumn <shinigami7x@proton.me>
2026-06-19 02:30:57 -0400

    Port ArmorUtil (#2623)
    * Updated ArmorUtil
    
    * Updated ArmorUtilTests
    
    * ArmorUtil refactor
    
    * ArmorUtilTests refactor
    
    * Change the name of `nonShroudedArmor` to `armorSlots` in `ArmorUtil.getShroudedArmorPercentage()`
    
    Signed-off-by: Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
    
    * Remove redundant `shroudedArmor > 0` check from numerator calculation in `getShroudedArmorPercentage()`.
    
    Signed-off-by: Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>
    Co-authored-by: Sleepy-Horse <97367938+Sleepy-Horse@users.noreply.github.com>

--- src/main/java/twilightforest/util/ArmorUtil.java
+++ src/main/java/twilightforest/util/ArmorUtil.java
@@ -1,30 +1,28 @@
 package twilightforest.util;
 
+import net.minecraft.world.entity.EquipmentSlot;
+import net.minecraft.world.entity.EquipmentSlotGroup;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.component.DyedItemColor;
 import tamaized.beanification.Component;
 import twilightforest.init.TFDataComponents;
-import twilightforest.item.ArcticArmorItem;
-
-import java.util.OptionalInt;
 
 @Component
 public class ArmorUtil {
-
 	public float getShroudedArmorPercentage(LivingEntity entity) {
 		int shroudedArmor = 0;
-		int nonShroudedArmor = 0;
+		int armorSlots = 0;
 
-		for (ItemStack stack : entity.getArmorSlots()) {
+		for (EquipmentSlot slot : EquipmentSlotGroup.ARMOR) {
+			ItemStack stack = entity.getItemBySlot(slot);
+
 			if (!stack.isEmpty() && stack.get(TFDataComponents.EMPERORS_CLOTH) != null) {
 				shroudedArmor++;
 			}
 
-			nonShroudedArmor++;
+			armorSlots++;
 		}
 
-		return nonShroudedArmor > 0 && shroudedArmor > 0 ? (float) shroudedArmor / (float) nonShroudedArmor : 0.0F;
+		return armorSlots > 0 ? (float) shroudedArmor / (float) armorSlots : 0.0F;
 	}
-
 }
--- src/test/java/twilightforest/util/ArmorUtilTests.java
+++ src/test/java/twilightforest/util/ArmorUtilTests.java
@@ -2,6 +2,7 @@ package twilightforest.util;
 
 import net.minecraft.core.component.DataComponentPatch;
 import net.minecraft.util.Unit;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -11,8 +12,6 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import tamaized.beanification.junit.MockitoFixer;
 import twilightforest.init.TFDataComponents;
 import twilightforest.init.TFItems;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
@@ -31,15 +30,29 @@ public class ArmorUtilTests {
 	@Test
 	public void getShroudedArmorPercentage() {
 		LivingEntity entity = mock(LivingEntity.class);
-		when(entity.getArmorSlots()).thenReturn(List.of(
-			ItemStack.EMPTY,
-			new ItemStack(Items.STICK),
-			new ItemStack(TFItems.ARCTIC_BOOTS, 1, DataComponentPatch.builder().set(TFDataComponents.EMPERORS_CLOTH.get(), Unit.INSTANCE).build())
-		));
+
+		when(entity.getItemBySlot(EquipmentSlot.HEAD))
+			.thenReturn(ItemStack.EMPTY);
+
+		when(entity.getItemBySlot(EquipmentSlot.CHEST))
+			.thenReturn(new ItemStack(Items.STICK));
+
+		when(entity.getItemBySlot(EquipmentSlot.LEGS))
+			.thenReturn(ItemStack.EMPTY);
+
+		when(entity.getItemBySlot(EquipmentSlot.FEET))
+			.thenReturn(
+				new ItemStack(
+					TFItems.ARCTIC_BOOTS,
+					1,
+					DataComponentPatch.builder()
+						.set(TFDataComponents.EMPERORS_CLOTH.get(), Unit.INSTANCE)
+						.build()
+				)
+			);
 
 		float result = instance.getShroudedArmorPercentage(entity);
 
-		assertEquals(1F / 3F, result);
+		assertEquals(0.25F, result);
 	}
-
 }
