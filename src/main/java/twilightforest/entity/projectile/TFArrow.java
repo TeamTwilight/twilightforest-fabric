a0bd313e7ec5250cae122852a57f6d4f4ecd074f
Autumn <shinigami7x@proton.me>
2026-07-23 12:06:36 -0400

    Uncomment AT entries and change annotation provider (#2675)

--- src/main/java/twilightforest/entity/projectile/TFArrow.java
+++ src/main/java/twilightforest/entity/projectile/TFArrow.java
@@ -6,7 +6,7 @@ import net.minecraft.world.entity.projectile.arrow.AbstractArrow;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
-import org.jetbrains.annotations.Nullable;
+import org.jspecify.annotations.Nullable;
 
 public abstract class TFArrow extends AbstractArrow implements ITFProjectile {
 
