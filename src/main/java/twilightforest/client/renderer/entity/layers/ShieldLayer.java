5feb8da3c2df79ea2ccffd303adbab98b8df934b
a6tumn <shinigami7x@proton.me>
2026-08-06 00:31:30 -0400

    Brought over Lich Shield fixes from Mitama's branch.

--- src/main/java/twilightforest/client/renderer/entity/layers/ShieldLayer.java
+++ src/main/java/twilightforest/client/renderer/entity/layers/ShieldLayer.java
@@ -7,7 +7,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.EntityModel;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.Sheets;
-import net.minecraft.client.renderer.block.model.BakedQuad;
 import net.minecraft.client.renderer.entity.RenderLayerParent;
 import net.minecraft.client.renderer.entity.layers.RenderLayer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
@@ -16,6 +15,7 @@ import net.minecraft.core.Direction;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.Mth;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.item.ItemStack;
 import org.apache.commons.lang3.ArrayUtils;
 import twilightforest.TwilightForestMod;
 import twilightforest.entity.boss.Lich;
@@ -33,7 +33,7 @@ public class ShieldLayer<T extends LivingEntity, M extends EntityModel<T>> exten
 	@Override
 	public void render(PoseStack stack, MultiBufferSource buffer, int light, T entity, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 		if (this.getShieldCount(entity) > 0) {
-			this.renderShields(stack, buffer, light, entity, partialTicks);
+			this.renderShields(stack, buffer, entity, partialTicks);
 		}
 	}
 
@@ -43,38 +43,37 @@ public class ShieldLayer<T extends LivingEntity, M extends EntityModel<T>> exten
 			: entity.getAttachedOrCreate(TFDataAttachments.FORTIFICATION_SHIELDS).shieldsLeft();
 	}
 
-	private void renderShields(PoseStack stack, MultiBufferSource buffer, int light, T entity, float partialTicks) {
+	private void renderShields(PoseStack stack, MultiBufferSource buffer, T entity, float partialTicks) {
 		float age = entity.tickCount + partialTicks;
-		float rotateAngleY = age / 5.0F;
+		float rotateAngleY = age / -5.0F;
 		float rotateAngleX = Mth.sin(age / 5.0F) / 4.0F;
 		float rotateAngleZ = Mth.cos(age / 5.0F) / 4.0F;
 
 		int count = getShieldCount(entity);
-		BakedModel model = Minecraft.getInstance().getModelManager().getModel(SHIELD_MODEL);
-
 		for (int c = 0; c < count; c++) {
 			stack.pushPose();
 
-			// shift to the center of the entity, invert Y to fix rendering direction
-			stack.translate(-0.5F, 0.5F, -0.5F);
-			stack.scale(1.0F, -1.0F, 1.0F);
-
 			// perform the rotations, accounting for the fact that baked models are corner-based
-			stack.translate(0.5F, 0.5F, 0.5F);
-			stack.mulPose(Axis.ZP.rotationDegrees(rotateAngleZ * (180.0F / Mth.PI)));
+			// Z gets extra 180 degrees to flip visual upside-down, since scaling y by -1 will cause back-faces to render instead
+			stack.mulPose(Axis.ZP.rotationDegrees(180.0F + rotateAngleZ * (180.0F / Mth.PI)));
 			stack.mulPose(Axis.YP.rotationDegrees(rotateAngleY * (180.0F / Mth.PI) + (c * (360.0F / count))));
 			stack.mulPose(Axis.XP.rotationDegrees(rotateAngleX * (180.0F / Mth.PI)));
-			stack.translate(-0.5F, -0.5F, -0.5F);
+			stack.translate(-0.5F, -0.65F, -0.5F);
 
 			// push the shields outwards from the center of rotation
 			stack.translate(0.0F, 0.0F, -0.7F);
 
+			BakedModel model = Minecraft.getInstance().getModelManager().getModel(SHIELD_MODEL);
 			VertexConsumer consumer = buffer.getBuffer(Sheets.translucentCullBlockSheet());
-			PoseStack.Pose pose = stack.last();
 			for (Direction dir : DIRS) {
-				for (BakedQuad quad : model.getQuads(null, dir, entity.getRandom())) {
-					consumer.putBulkData(pose, quad, 1.0F, 1.0F, 1.0F, 1.0F, light, OverlayTexture.NO_OVERLAY);
-				}
+				Minecraft.getInstance().getItemRenderer().renderQuadList(
+					stack,
+					consumer,
+					model.getQuads(null, dir, entity.getRandom()),
+					ItemStack.EMPTY,
+					0xF000F0,
+					OverlayTexture.NO_OVERLAY
+				);
 			}
 
 			stack.popPose();
