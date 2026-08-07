424abc0ee887b166061c81c03a42593446d203fb
a6tumn <shinigami7x@proton.me>
2026-08-07 00:08:54 -0400

    Fixed Maze Slime Block piston behavior.

--- src/main/java/twilightforest/block/MazeSlimeBlock.java
+++ src/main/java/twilightforest/block/MazeSlimeBlock.java
@@ -1,12 +1,24 @@
 package twilightforest.block;
 
 import net.minecraft.world.level.block.SlimeBlock;
+import io.github.fabricators_of_create.porting_lib.blocks.extensions.StickToBlock;
+import io.github.fabricators_of_create.porting_lib.blocks.extensions.StickyBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import twilightforest.data.tags.BlockTagGenerator;
+import twilightforest.init.TFBlocks;
 
-public class MazeSlimeBlock extends SlimeBlock {
+public class MazeSlimeBlock extends SlimeBlock implements StickToBlock, StickyBlock {
 	public MazeSlimeBlock(Properties properties) {
 		super(properties);
 	}
 
-	// canStickTo is NeoForge-only; PistonBaseBlock.isSticky() handled via PistonBaseBlockMixin
-	// isStickyBlock is inherited from SlimeBlock (returns true)
+	@Override
+	public boolean canStickTo(BlockState state, BlockState other) {
+		return other.is(TFBlocks.MAZE_SLIME_BLOCK) || other.is(BlockTagGenerator.MAZESTONE);
+	}
+
+	@Override
+	public boolean isStickyBlock(BlockState state) {
+		return true;
+	}
 }
