4878a55067114302966e88894ced095a4f28fe1d
a6tumn <shinigami7x@proton.me>
2026-08-19 18:07:12 -0400

    Minor improvements.

--- src/main/java/twilightforest/mixin/BlockEntityTypeMixin.java
+++ src/main/java/twilightforest/mixin/BlockEntityTypeMixin.java
@@ -1,20 +1,15 @@
 package twilightforest.mixin;
 
 import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import twilightforest.TwilightForestMod;
 
-/**
- * Mixin to allow Twilight Forest chest blocks to use vanilla ChestBlockEntity.
- * In 1.21.1, BlockEntityType.isValid() checks if the block state is in the type's
- * supported blocks set. TF custom chests (dark_chest, mangrove_chest, etc.) inherit
- * ChestBlock but are not in BlockEntityType.CHEST's supported blocks, causing
- * IllegalStateException when Boss loot chests are placed.
- */
 @Mixin(BlockEntityType.class)
 public class BlockEntityTypeMixin {
 
@@ -27,16 +22,10 @@ public class BlockEntityTypeMixin {
 		BlockState state,
 		CallbackInfoReturnable<Boolean> cir
 	) {
-		// If already valid, don't interfere
 		if (cir.getReturnValue()) return;
 
-		// Allow TF chest/trapped chest blocks to be used with vanilla chest block entity types
-		String blockName = BuiltInRegistries.BLOCK.getKey(state.getBlock()).toString();
-		if (blockName.startsWith("twilightforest:") &&
-			(blockName.endsWith("_chest") ||
-				blockName.endsWith("_trapped_chest") ||
-				blockName.endsWith("_sign") ||
-				blockName.endsWith("_wall_sign"))) {
+		ResourceLocation id = BuiltInRegistries.BLOCK.getKey(state.getBlock());
+		if (id.getNamespace().equals(TwilightForestMod.ID) && (id.getPath().endsWith("_chest") || id.getPath().endsWith("_trapped_chest"))) {
 			cir.setReturnValue(true);
 		}
 	}
