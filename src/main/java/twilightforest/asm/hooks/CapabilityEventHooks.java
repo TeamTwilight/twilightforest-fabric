784d93ed464f85d702426499a7bf9f26c8e7160e
a6tumn <shinigami7x@proton.me>
2026-08-23 22:09:10 -0400

    Ported a missed event in CapabilityEvents to Fabric.

--- src/main/java/twilightforest/asm/hooks/CapabilityEventHooks.java
+++ src/main/java/twilightforest/asm/hooks/CapabilityEventHooks.java
@@ -1,9 +1,7 @@
 package twilightforest.asm.hooks;
 
 import net.minecraft.server.level.ServerPlayer;
-import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.world.entity.LivingEntity;
-import twilightforest.components.entity.FortificationShieldAttachment;
 import twilightforest.events.CapabilityEvents;
 import twilightforest.init.TFDataAttachments;
 
@@ -29,26 +27,11 @@ public final class CapabilityEventHooks {
 		event.getEntity().getData(TFDataAttachments.TF_PORTAL_COOLDOWN).tick(event.getEntity());
 	}
 
-	private void absorbShieldHits(LivingIncomingDamageEvent event) {
-		LivingEntity living = event.getEntity();
-		if (!living.level().isClientSide() && !event.getSource().is(DamageTypeTags.BYPASSES_ARMOR)) {
-			FortificationShieldAttachment attachment = living.getData(TFDataAttachments.FORTIFICATION_SHIELDS);
-			if (attachment.shieldsLeft() > 0) {
-				if (living.invulnerableTime <= 0) {
-					attachment.breakShield(living, false);
-					FortificationShieldAttachment.addShieldBreakParticles(event.getSource(), living);
-					living.invulnerableTime = living.invulnerableDuration;
-				}
-				event.setCanceled(true);
-			}
-		}
-	}
-
 	public void playerLogsIn(PlayerEvent.PlayerLoggedInEvent event) {
 		if (event.getEntity().level().isClientSide() || !(event.getEntity() instanceof ServerPlayer player))
 			return;
 		if (!player.hasData(TFDataAttachments.BANISHED_TO_TWILIGHT_FOREST))
 			CapabilityEvents.newSpawnInTwilightForest(player);
 	}
-	*/
+	 */
 }
--- src/main/java/twilightforest/events/CapabilityEvents.java
+++ src/main/java/twilightforest/events/CapabilityEvents.java
@@ -1,12 +1,17 @@
 package twilightforest.events;
 
+import net.fabricmc.fabric.api.entity.event.v1.ServerLivingEntityEvents;
 import net.fabricmc.fabric.api.entity.event.v1.ServerPlayerEvents;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.util.Unit;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.storage.LevelData;
+import twilightforest.components.entity.FortificationShieldAttachment;
 import twilightforest.config.TFConfig;
 import twilightforest.init.TFDataAttachments;
 import twilightforest.init.TFDimension;
@@ -17,7 +22,23 @@ public class CapabilityEvents {
 	public static final CapabilityEvents INSTANCE = new CapabilityEvents();
 
 	public static void init() {
+		ServerLivingEntityEvents.ALLOW_DAMAGE.register(INSTANCE::absorbShieldHits);
 		ServerPlayerEvents.AFTER_RESPAWN.register((_, newPlayer, _) -> INSTANCE.spawnInTFIfNecessary(newPlayer));
+	}
+
+	private boolean absorbShieldHits(LivingEntity entity, DamageSource source, float amount) {
+		if (!entity.level().isClientSide() && !source.is(DamageTypeTags.BYPASSES_ARMOR)) {
+			FortificationShieldAttachment attachment = entity.getAttached(TFDataAttachments.FORTIFICATION_SHIELDS);
+			if (attachment.shieldsLeft() > 0) {
+				if (entity.invulnerableTime <= 0) {
+					attachment.breakShield(entity, false);
+					FortificationShieldAttachment.addShieldBreakParticles(source, entity);
+					entity.invulnerableTime = LivingEntity.DEATH_DURATION;
+				}
+				return false;
+			}
+		}
+		return true;
 	}
 
 	private void spawnInTFIfNecessary(ServerPlayer newPlayer) {
