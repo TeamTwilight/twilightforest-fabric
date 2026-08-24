aeee997d9f454179f52c96c157d92d94311dc32a
a6tumn <shinigami7x@proton.me>
2026-08-23 22:37:09 -0400

    Ported MiscEvents to Fabric.

--- src/main/java/twilightforest/events/MiscEvents.java
+++ src/main/java/twilightforest/events/MiscEvents.java
@@ -1,10 +1,15 @@
 package twilightforest.events;
 
+import carminite.network.PacketDistributor;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.*;
 import net.minecraft.world.entity.ai.goal.target.NearestAttackableTargetGoal;
@@ -19,15 +24,7 @@ import net.minecraft.world.level.block.LecternBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.gameevent.GameEvent;
-import net.neoforged.fml.ModList;
-import net.neoforged.neoforge.common.NeoForge;
-import net.neoforged.neoforge.event.entity.EntityJoinLevelEvent;
-import net.neoforged.neoforge.event.entity.living.LivingEquipmentChangeEvent;
-import net.neoforged.neoforge.event.entity.player.PlayerInteractEvent;
-import net.neoforged.neoforge.network.PacketDistributor;
-import tamaized.beanification.Component;
-import tamaized.beanification.PostConstruct;
-import twilightforest.compat.curios.CuriosCompat;
+import net.minecraft.world.phys.BlockHitResult;
 import twilightforest.entity.monster.DeathTome;
 import twilightforest.entity.passive.Bighorn;
 import twilightforest.entity.passive.DwarfRabbit;
@@ -38,19 +35,18 @@ import twilightforest.init.TFDataComponents;
 import twilightforest.init.TFEntities;
 import twilightforest.network.CreateMovingCicadaSoundPacket;
 
-@Component
 public class MiscEvents {
+	public static final MiscEvents INSTANCE = new MiscEvents();
 
-	@PostConstruct
-	private void setup() {
-		NeoForge.EVENT_BUS.addListener(this::addPrey);
-		NeoForge.EVENT_BUS.addListener(this::updateCicadaSoundsOnHead);
-		NeoForge.EVENT_BUS.addListener(this::addTomesToLecterns);
-		NeoForge.EVENT_BUS.addListener(this::washOffCloth);
+	private void init() {
+		ServerEntityEvents.ENTITY_LOAD.register((entity, _) -> INSTANCE.addPrey(entity));
+		ServerEntityEvents.EQUIPMENT_CHANGE.register((livingEntity, equipmentSlot, _, currentStack) -> INSTANCE.updateCicadaSoundsOnHead(livingEntity, equipmentSlot, currentStack));
+		UseBlockCallback.EVENT.register(INSTANCE::addTomesToLecterns);
+		UseBlockCallback.EVENT.register(INSTANCE::washOffCloth);
 	}
 
-	private void addPrey(EntityJoinLevelEvent event) {
-		if (event.getEntity() instanceof Mob mob) {
+	private void addPrey(Entity entity) {
+		if (entity instanceof Mob mob) {
 			EntityType<?> type = mob.getType();
 			if (type == EntityType.CAT) {
 				mob.targetSelector.addGoal(1, new NonTameRandomTargetGoal<>((TamableAnimal) mob, DwarfRabbit.class, true, null));
@@ -71,58 +67,71 @@ public class MiscEvents {
 		}
 	}
 
-	private void updateCicadaSoundsOnHead(LivingEquipmentChangeEvent event) {
-		LivingEntity living = event.getEntity();
-
+	private void updateCicadaSoundsOnHead(LivingEntity livingEntity, EquipmentSlot equipmentSlot, ItemStack currentStack) {
 		// from what I can see, vanilla doesn't have a hook for this in the item class. So this will have to do.
 		// we only have to check equipping, when its unequipped the sound instance handles the rest
 
 		//if we have a cicada in our curios slot, don't try to run this
-		 if (ModList.get().isLoaded("curios")) {
-		 	if (CuriosCompat.isCurioEquipped(living, stack -> stack.is(TFBlocks.CICADA.asItem()))) return;
+		 if (FabricLoader.getInstance().isModLoaded("trinkets")) {
+		 	//if (CuriosCompat.isCurioEquipped(living, stack -> stack.is(TFBlocks.CICADA.asItem()))) return;
 		 }
 
-		if (!living.level().isClientSide() && event.getSlot() == EquipmentSlot.HEAD && event.getTo().is(TFBlocks.CICADA.asItem())) {
-			PacketDistributor.sendToPlayersTrackingEntityAndSelf(living, new CreateMovingCicadaSoundPacket(living.getId()));
+		if (!livingEntity.level().isClientSide() && equipmentSlot == EquipmentSlot.HEAD && currentStack.is(TFBlocks.CICADA.asItem())) {
+			PacketDistributor.sendToPlayersTrackingEntityAndSelf(livingEntity, new CreateMovingCicadaSoundPacket(livingEntity.getId()));
 		}
 	}
 
-	private void addTomesToLecterns(PlayerInteractEvent.RightClickBlock event) {
-		Player player = event.getEntity();
-		ItemStack stack = player.getItemInHand(event.getHand());
+	private InteractionResult addTomesToLecterns(Player player, Level level, InteractionHand hand, BlockHitResult hitResult) {
+		if (player.isSpectator()) {
+			return InteractionResult.PASS;
+		}
 
-		if (!(stack.getItem() instanceof SpawnEggItem spawnEggItem) || spawnEggItem.getType(stack) != TFEntities.DEATH_TOME.get())
-			return;
+		ItemStack stack = player.getItemInHand(hand);
 
-		BlockPos pos = event.getPos();
-		Level level = event.getLevel();
+		if (!(stack.getItem() instanceof SpawnEggItem) || SpawnEggItem.getType(stack) != TFEntities.DEATH_TOME)
+			return InteractionResult.PASS;
+
+		BlockPos pos = hitResult.getBlockPos();
 		BlockState state = level.getBlockState(pos);
 
 		if (state.getBlock() instanceof LecternBlock && !state.getValue(BlockStateProperties.HAS_BOOK)) {
-			event.setCanceled(true);
 			level.playSound(null, pos, SoundEvents.BOOK_PUT, SoundSource.BLOCKS, 1.0F, 1.0F);
 
 			if (level instanceof ServerLevel serverLevel) {
-				DeathTome tome = TFEntities.DEATH_TOME.get().spawn(serverLevel, stack, player, pos.below(), EntitySpawnReason.SPAWN_EGG, true, false);
+				DeathTome tome = TFEntities.DEATH_TOME.spawn(serverLevel, stack, player, pos.below(), EntitySpawnReason.SPAWN_ITEM_USE, true, false);
 				if (tome != null) {
 					stack.consume(1, player);
 					serverLevel.gameEvent(player, GameEvent.ENTITY_PLACE, pos);
 					tome.setOnLectern(true);
 				}
 			}
+
+			return InteractionResult.SUCCESS;
 		}
+
+		return InteractionResult.PASS;
 	}
 
-	private void washOffCloth(PlayerInteractEvent.RightClickBlock event) {
-		if (event.isCanceled()) return;
-		BlockState state = event.getLevel().getBlockState(event.getPos());
-		if (!state.is(Blocks.WATER_CAULDRON) || state.getValue(LayeredCauldronBlock.LEVEL) <= 0) return;
-		if (event.getItemStack().has(TFDataComponents.EMPERORS_CLOTH)) {
-			LayeredCauldronBlock.lowerFillLevel(state, event.getLevel(), event.getPos());
-			event.getItemStack().remove(TFDataComponents.EMPERORS_CLOTH);
-			event.getEntity().awardStat(Stats.CLEAN_ARMOR);
-			event.setCancellationResult(InteractionResult.SUCCESS);
-			event.setCanceled(true);
+	private InteractionResult washOffCloth(Player player, Level level, InteractionHand hand, BlockHitResult hitResult) {
+		if (player.isSpectator()) {
+			return InteractionResult.PASS;
 		}
+
+		BlockPos pos = hitResult.getBlockPos();
+		BlockState state = level.getBlockState(pos);
+		ItemStack usedItem = player.getItemInHand(hand);
+
+		if (!state.is(Blocks.WATER_CAULDRON) || state.getValue(LayeredCauldronBlock.LEVEL) <= 0) {
+			return InteractionResult.PASS;
+		}
+
+		if (usedItem.has(TFDataComponents.EMPERORS_CLOTH)) {
+			LayeredCauldronBlock.lowerFillLevel(state, level, pos);
+			usedItem.remove(TFDataComponents.EMPERORS_CLOTH);
+			player.awardStat(Stats.CLEAN_ARMOR);
+			return InteractionResult.SUCCESS;
+		}
+
+		return InteractionResult.PASS;
 	}
 }
