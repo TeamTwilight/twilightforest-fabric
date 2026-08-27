ccd1d20139a7c2382244d421ce55ef70be3177d0
zyrsky <zyrsky888@163.com>
2026-08-28 02:09:09 +0800

    Keep NoopModel's renderToBuffer override via extendable access widener
    Per review: instead of dropping the empty override, widen
    Model.renderToBuffer to be extensible in the classtweaker so the override
    can stay, matching NeoForge. Can be re-evaluated once the mod compiles.

--- src/main/java/twilightforest/client/model/entity/NoopModel.java
+++ src/main/java/twilightforest/client/model/entity/NoopModel.java
@@ -1,5 +1,7 @@
 package twilightforest.client.model.entity;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
@@ -8,5 +10,9 @@ public class NoopModel<T extends HumanoidRenderState> extends HumanoidModel<T> {
 
 	public NoopModel(ModelPart part) {
 		super(part);
+	}
+
+	@Override
+	public void renderToBuffer(PoseStack ms, VertexConsumer buffers, int light, int overlay, int color) {
 	}
 }
