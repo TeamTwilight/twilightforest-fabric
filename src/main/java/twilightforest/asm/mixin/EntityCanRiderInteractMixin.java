68a6e6bed6e06361245c4d483a9acdb03c7ca8bc
zyrsky <zyrsky888@163.com>
2026-08-28 02:37:48 +0800

    Use carminite-lib's IEntityExtension.canRiderInteract
    carminite-lib now injects IEntityExtension (default canRiderInteract() =
    false) onto Entity and intercepts Entity.interact to allow rider
    interactions. Drop the local CanRiderInteract/EntityInteractMixin mixins
    and bump the submodule to the merged commit; AlphaYeti, PinchBeetle and
    Yeti keep their canRiderInteract() returning true.

--- src/main/java/twilightforest/asm/mixin/EntityCanRiderInteractMixin.java
+++
@@ -1,8 +0,0 @@
-package twilightforest.asm.mixin;
-
-import net.minecraft.world.entity.Entity;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(Entity.class)
-public class EntityCanRiderInteractMixin implements CanRiderInteract {
-}
