704dd355de8501113078641c8c1b24bde00b8d56
a6tumn <shinigami7x@proton.me>
2026-08-27 12:27:35 -0400

    Restored curios/trinkets checks.

--- src/main/java/twilightforest/client/MovingCicadaSoundInstance.java
+++ src/main/java/twilightforest/client/MovingCicadaSoundInstance.java
@@ -1,5 +1,6 @@
 package twilightforest.client;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
 import net.minecraft.client.resources.sounds.SoundInstance;
 import net.minecraft.sounds.SoundSource;
@@ -25,13 +26,20 @@ public class MovingCicadaSoundInstance extends AbstractTickableSoundInstance {
 
 	@Override
 	public void tick() {
-		if (!this.wearer.isRemoved() && this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem())) {
+		if (!this.wearer.isRemoved() && (this.wearer.getItemBySlot(EquipmentSlot.HEAD).is(TFBlocks.CICADA.asItem()) || this.isWearingCicadaTrinket())) {
 			this.x = (float) this.wearer.getX();
 			this.y = (float) this.wearer.getY();
 			this.z = (float) this.wearer.getZ();
 		} else {
 			this.stop();
 		}
+	}
+
+	private boolean isWearingCicadaTrinket() {
+		if (FabricLoader.getInstance().isModLoaded("trinkets")) {
+			//return CuriosCompat.isCurioEquipped(this.wearer, stack -> stack.is(TFBlocks.CICADA.asItem()));
+		}
+		return false;
 	}
 
 	@Override
