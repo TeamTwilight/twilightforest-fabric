5b8e63305ff3405c80a1ec127460afb1099bd95e
zyrsky <zyrsky888@163.com>
2026-08-28 01:18:21 +0800

    Adapt EntityHooks to 26.1/Fabric APIs
    - FluidType/NeoForgeMod APIs are gone: use isInWater() for the water
      walking splash check and drop the unrestrainedSwimPredicate hook (its
      isInFluidType injection point no longer exists)
    - mob.hasData -> hasAttached (Fabric attachment API)
    - widen Entity.stuckSpeedMultiplier via the classtweaker

--- src/main/java/twilightforest/asm/hooks/coremod/EntityHooks.java
+++ src/main/java/twilightforest/asm/hooks/coremod/EntityHooks.java
@@ -7,14 +7,11 @@ import net.minecraft.world.entity.MoverType;
 import net.minecraft.world.entity.PathfinderMob;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.phys.Vec3;
-import net.neoforged.neoforge.common.NeoForgeMod;
-import net.neoforged.neoforge.fluids.FluidType;
 import org.jetbrains.annotations.Nullable;
 import twilightforest.init.TFDataAttachments;
 import twilightforest.init.custom.TravellersModifiersManager;
 import twilightforest.item.travellers_gear.TravellersGearLogic;
 
-import java.util.function.BiPredicate;
 
 @SuppressWarnings({"JavadocReference", "unused"})
 public class EntityHooks {
@@ -34,7 +31,7 @@ public class EntityHooks {
 			return o;
 
 		boolean isWaterWalking = TravellersGearLogic.isBelowMaxWaterWalkingSubmergedHeight(livingEntity) && !livingEntity.isShiftKeyDown();
-		if (livingEntity.getFluidTypeHeight(NeoForgeMod.WATER_TYPE.value()) > 0 && isWaterWalking && livingEntity.level().getGameTime() % 3 == 1)
+		if (livingEntity.isInWater() && isWaterWalking && livingEntity.level().getGameTime() % 3 == 1)
 			TravellersGearLogic.waterWalkingSplashEffect(livingEntity);
 		return isWaterWalking;
 	}
@@ -53,23 +50,6 @@ public class EntityHooks {
 	}
 
 	/**
-	 * {@link twilightforest.asm.transformers.entity.WaterSprintTransformer}<p/>
-	 * <p>
-	 * Injection Point:<br/>
-	 * {@link net.minecraft.client.player.LocalPlayer#aiStep()}
-	 * Targets: {@link net.neoforged.neoforge.common.extensions.IEntityExtension#isInFluidType(java.util.function.BiPredicate<net.neoforged.neoforge.fluids.FluidType, Double>)}
-	 */
-	public static BiPredicate<FluidType, Double> unrestrainedSwimPredicate(BiPredicate<FluidType, Double> o, LivingEntity livingEntity) {
-		return (fluidType, height) -> {
-			FluidState fs = livingEntity.level().getFluidState(livingEntity.blockPosition());
-			boolean oResult = o.test(fluidType, height);
-			if (fluidType != NeoForgeMod.WATER_TYPE.value())
-				return oResult;
-			return unrestrainedSprintingInWater(oResult, livingEntity);
-		};
-	}
-
-	/**
 	 * {@link twilightforest.asm.transformers.entity.PathFinderUnrestrainedByLeashTransformer} <p/>
 	 *
 	 * Injection Point:<br/>
@@ -77,7 +57,7 @@ public class EntityHooks {
 	 * Targets: IRETURN
 	 */
 	public static boolean overrideStayCloseToHolder(boolean prior, PathfinderMob mob) {
-		return prior && !mob.hasData(TFDataAttachments.LEASH_PATHFINDER_OVERRIDE);
+		return prior && !mob.hasAttached(TFDataAttachments.LEASH_PATHFINDER_OVERRIDE);
 	}
 
 	/**
