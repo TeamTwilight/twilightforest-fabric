7cb256b218519ba08e47510673e4b0d6c796318a
Autumn <shinigami7x@proton.me>
2026-09-06 05:08:59 -0400

    Initial port of base armor renderers.

--- src/main/java/twilightforest/client/renderer/armor/TFArmorRenderer.java
+++ src/main/java/twilightforest/client/renderer/armor/TFArmorRenderer.java
@@ -1,23 +1,27 @@
 package twilightforest.client.renderer.armor;
 
+import carminite.util.Lazy;
+import net.fabricmc.fabric.api.client.rendering.v1.ArmorRenderer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.layers.EquipmentLayerRenderer;
 import net.minecraft.server.packs.resources.ResourceManager;
 import net.minecraft.server.packs.resources.ResourceManagerReloadListener;
-import net.neoforged.neoforge.client.extensions.common.IClientItemExtensions;
-import net.neoforged.neoforge.common.util.Lazy;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public abstract class TFArmorRenderer implements IClientItemExtensions {
+public abstract class TFArmorRenderer implements ArmorRenderer {
 	public static final List<TFSimpleArmorRenderer> INSTANCES = new ArrayList<>();
 	protected final Map<ModelLayerLocation, Lazy<ModelPart>> ARMOR_MODELS = new HashMap<>();
+	protected final EquipmentLayerRenderer equipmentRenderer;
 
-	public TFArmorRenderer(ModelLayerLocation... layerLocations) {
+	public TFArmorRenderer(EntityRendererProvider.Context context, ModelLayerLocation... layerLocations) {
+		this.equipmentRenderer = context.getEquipmentRenderer();
 		for (ModelLayerLocation layerLocation : layerLocations) {
 			this.ARMOR_MODELS.put(
 				layerLocation,
--- src/main/java/twilightforest/client/renderer/armor/TFSimpleArmorRenderer.java
+++ src/main/java/twilightforest/client/renderer/armor/TFSimpleArmorRenderer.java
@@ -1,10 +1,15 @@
 package twilightforest.client.renderer.armor;
 
-import net.minecraft.client.model.Model;
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.model.HumanoidModel;
 import net.minecraft.client.model.geom.ModelLayerLocation;
 import net.minecraft.client.model.geom.ModelPart;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.client.renderer.entity.state.HumanoidRenderState;
 import net.minecraft.client.resources.model.EquipmentClientInfo;
 import net.minecraft.core.component.DataComponents;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.equipment.Equippable;
@@ -17,22 +22,46 @@ public class TFSimpleArmorRenderer extends TFArmorRenderer {
 	protected final ModelLayerLocation INNER_ARMOR_MODEL;
 	protected final ModelLayerLocation OUTER_ARMOR_MODEL;
 
-	public TFSimpleArmorRenderer(Function<ModelPart, TFArmorModel> createModelInstance, ModelLayerLocation innerLayerLocation, ModelLayerLocation outerLayerLocation) {
-		super(innerLayerLocation, outerLayerLocation);
+	public TFSimpleArmorRenderer(EntityRendererProvider.Context context, Function<ModelPart, TFArmorModel> createModelInstance, ModelLayerLocation innerLayerLocation, ModelLayerLocation outerLayerLocation) {
+		super(context, innerLayerLocation, outerLayerLocation);
+
 		this.INNER_ARMOR_MODEL = innerLayerLocation;
 		this.OUTER_ARMOR_MODEL = outerLayerLocation;
 		this.CREATE_MODEL_INSTANCE = createModelInstance;
+
+		INSTANCES.add(this);
 	}
 
 	@Override
-	public Model<?> getHumanoidArmorModel(ItemStack itemStack, EquipmentClientInfo.LayerType layerType, @SuppressWarnings("rawtypes") Model original) {
-		Equippable equippable = itemStack.get(DataComponents.EQUIPPABLE);
-		if (equippable == null) return super.getHumanoidArmorModel(itemStack, layerType, original);
+	public void render(PoseStack poseStack, SubmitNodeCollector submitNodeCollector, ItemStack stack, HumanoidRenderState state, EquipmentSlot slot, int light, HumanoidModel<HumanoidRenderState> contextModel) {
+		Equippable equippable = stack.get(DataComponents.EQUIPPABLE);
+		if (equippable == null || equippable.assetId().isEmpty() || equippable.slot() != slot) {
+			return;
+		}
 
-		EquipmentSlot slot = equippable.slot();
-		TFArmorModel model = this.CREATE_MODEL_INSTANCE.apply(this.getModelPart(slot == EquipmentSlot.LEGS ? this.INNER_ARMOR_MODEL : this.OUTER_ARMOR_MODEL));
+		TFArmorModel model = this.CREATE_MODEL_INSTANCE.apply(
+			this.getModelPart(slot == EquipmentSlot.LEGS ? this.INNER_ARMOR_MODEL : this.OUTER_ARMOR_MODEL)
+		);
+
 		model.setSlot(slot);
+		EquipmentClientInfo.LayerType layerType =
+			state.isBaby && state.entityType != EntityType.ARMOR_STAND
+				? EquipmentClientInfo.LayerType.HUMANOID_BABY
+				: slot == EquipmentSlot.LEGS
+				? EquipmentClientInfo.LayerType.HUMANOID_LEGGINGS
+				: EquipmentClientInfo.LayerType.HUMANOID;
 
-		return model;
+		model.setupAnim(state);
+		equipmentRenderer.renderLayers(
+			layerType,
+			equippable.assetId().orElseThrow(),
+			model,
+			state,
+			stack,
+			poseStack,
+			submitNodeCollector,
+			light,
+			state.outlineColor
+		);
 	}
 }
