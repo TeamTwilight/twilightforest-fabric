8eaec0226738b9a852f2b7f56f78a612d72c8f07
Autumn <shinigami7x@proton.me>
2026-09-10 10:29:46 -0400

    Brought over altered version of 0eee966.

--- src/main/java/twilightforest/client/event/FogHandler.java
+++ src/main/java/twilightforest/client/event/FogHandler.java
@@ -4,22 +4,40 @@ import carminite.events.modified.CarminiteComputeFogColorEvent;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.util.Mth;
+import org.jspecify.annotations.Nullable;
+import twilightforest.init.TFBiomes;
 import twilightforest.init.TFDimension;
 
 public class FogHandler {
 	public static final FogHandler INSTANCE = new FogHandler();
 
+	private static float spookyPercent = 0.0F;
+
 	public void colorFog(CarminiteComputeFogColorEvent event) {
 		if (event.getCamera().entity() instanceof LocalPlayer player && player.level() instanceof ClientLevel client && client.dimension() == TFDimension.DIMENSION_KEY) {
+			float[] colors = new float[]{event.getRed(), event.getGreen(), event.getBlue()};
+			boolean spooky = isSpooky(client, player);
+
 			double time = 13000;
 			double d0 = Mth.frac(time / (double)24000.0F - (double)0.25F);
 			double d1 = (double)0.5F - Math.cos(d0 * Math.PI) / (double)2.0F;
 			double d2 = (float)(d0 * (double)2.0F + d1) / 3.0F;
 			float daylight = Mth.clamp(Mth.cos(d2 * (float) (Math.PI * 2)) * 2.0F + 0.5F, 0.0F, 1.0F);
 
-			event.setRed(event.getRed() * (daylight * 0.94F + 0.06F));
-			event.setGreen(event.getGreen() * (daylight * 0.94F + 0.06F));
-			event.setBlue(event.getBlue() * (daylight * 0.91F + 0.09F));
+			if (spooky) {
+				spookyPercent += 0.005F;
+			} else {
+				spookyPercent -= 0.005F;
+			}
+			spookyPercent = Mth.clamp(spookyPercent, 0F, 1F);
+
+			event.setRed(Mth.clampedLerp(spookyPercent, colors[0] * daylight * 0.94F + 0.06F, colors[0]));
+			event.setGreen(Mth.clampedLerp(spookyPercent, colors[1] * daylight * 0.94F + 0.06F, colors[1]));
+			event.setBlue(Mth.clampedLerp(spookyPercent, colors[2] * daylight * 0.91F + 0.09F, colors[2]));
 		}
+	}
+
+	private static boolean isSpooky(@Nullable ClientLevel level, @Nullable LocalPlayer player) {
+		return level != null && player != null && level.getBiome(player.blockPosition()).is(TFBiomes.SPOOKY_FOREST);
 	}
 }
