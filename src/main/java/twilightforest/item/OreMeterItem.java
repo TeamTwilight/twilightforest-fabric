4999972959e516a52e4cd78f1164d54fe7f0c3b7
Tamaized <9671313+Tamaized@users.noreply.github.com>
2026-09-11 09:56:50 -0400

    Fix OreMeterItem#appendHoverText localization

--- src/main/java/twilightforest/item/OreMeterItem.java
+++ src/main/java/twilightforest/item/OreMeterItem.java
@@ -147,7 +147,7 @@ public class OreMeterItem extends Item implements IContinuousUseItem {
 		Block block = stack.get(TFDataComponents.ORE_FILTER);
 
 		if (block != null)
-			builder.accept(Component.translatable("misc.twilightforest.ore_meter_targeted_block", block.getDescriptionId()).withStyle(ChatFormatting.GRAY));
+			builder.accept(Component.translatable("misc.twilightforest.ore_meter_targeted_block", Component.translatable(block.getDescriptionId())).withStyle(ChatFormatting.GRAY));
 
 		super.appendHoverText(stack, context, display, builder, flag);
 	}
