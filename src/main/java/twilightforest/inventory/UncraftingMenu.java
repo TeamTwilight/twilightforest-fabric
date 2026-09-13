2d52fcc94e3fb6149d7c4d91abacc562f8a20b81
Autumn <shinigami7x@proton.me>
2026-09-13 17:28:57 -0400

    Brought over altered version of 9278ea3.

--- src/main/java/twilightforest/inventory/UncraftingMenu.java
+++ src/main/java/twilightforest/inventory/UncraftingMenu.java
@@ -1,5 +1,6 @@
 package twilightforest.inventory;
 
+import it.unimi.dsi.fastutil.ints.IntList;
 import it.unimi.dsi.fastutil.objects.Object2IntMap;
 import net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags;
 import net.fabricmc.loader.api.FabricLoader;
@@ -605,11 +606,20 @@ public class UncraftingMenu extends AbstractCraftingMenu {
 	}
 
 	private ItemStack[] getIngredients(CraftingRecipe recipe) {
-		List<Ingredient> ingredients = recipe.placementInfo().ingredients();
-		ItemStack[] stacks = new ItemStack[ingredients.size()];
+		PlacementInfo placement = recipe.placementInfo();
+		List<Ingredient> ingredients = placement.ingredients();
+		IntList slots = placement.slotsToIngredientIndex();
+		ItemStack[] stacks = new ItemStack[slots.size()];
 
-		for (int i = 0; i < ingredients.size(); i++) {
-			ItemStack[] matchingStacks = ingredients.get(i).values.stream().filter(s -> !s.is(TFItemTags.BANNED_UNCRAFTING_INGREDIENTS)).map(p -> new ItemStack(p.value())).toArray(ItemStack[]::new);
+		for (int i = 0; i < slots.size(); i++) {
+			int index = slots.getInt(i);
+
+			if (index == PlacementInfo.EMPTY_SLOT) {
+				stacks[i] = ItemStack.EMPTY;
+				continue;
+			}
+
+			ItemStack[] matchingStacks = ingredients.get(index).values.stream().filter(s -> !s.is(TFItemTags.BANNED_UNCRAFTING_INGREDIENTS)).map(p -> new ItemStack(p.value())).toArray(ItemStack[]::new);
 			stacks[i] = matchingStacks.length > 0 ? matchingStacks[Math.floorMod(this.ingredientsInCycle, matchingStacks.length)] : ItemStack.EMPTY;
 		}
 
