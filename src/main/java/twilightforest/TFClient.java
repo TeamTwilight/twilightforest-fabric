e84e92960bc637511f2507188e242642ab6538aa
Autumn <shinigami7x@proton.me>
2026-09-15 18:55:27 -0400

    Moved event registration into mod initializers.

--- src/main/java/twilightforest/TFClient.java
+++ src/main/java/twilightforest/TFClient.java
@@ -1,11 +1,19 @@
 package twilightforest;
 
+import carminite.events.api.ClientEvents;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.fabricmc.fabric.api.client.model.loading.v1.ModelLoadingPlugin;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.client.particle.v1.FabricSpriteSet;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleProviderRegistry;
 import net.fabricmc.fabric.api.client.rendering.v1.*;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudElementRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.HudStatusBarHeightRegistry;
+import net.fabricmc.fabric.api.client.rendering.v1.hud.VanillaHudElements;
+import net.fabricmc.fabric.api.client.rendering.v1.level.LevelRenderEvents;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.fabricmc.fabric.api.resource.v1.ResourceLoader;
 import net.fabricmc.fabric.impl.client.model.loading.CustomUnbakedBlockStateModelRegistry;
 import net.minecraft.client.gui.screens.MenuScreens;
@@ -32,6 +40,7 @@ import net.minecraft.resources.Identifier;
 import net.minecraft.server.packs.PackType;
 import net.minecraft.world.level.saveddata.maps.MapDecorationTypes;
 import twilightforest.client.*;
+import twilightforest.client.listeners.*;
 import twilightforest.client.model.TFModelLayers;
 import twilightforest.client.model.TFModelLoadingPlugin;
 import twilightforest.client.model.armor.*;
@@ -44,6 +53,8 @@ import twilightforest.client.model.entity.*;
 import twilightforest.client.model.item.AnimatedItemModel;
 import twilightforest.client.model.item.TravellersGearItemModel;
 import twilightforest.client.model.item.TrollsteinnItemModel;
+import twilightforest.client.overlay.ItemDisplayOverlay;
+import twilightforest.client.overlay.PortalOverlay;
 import twilightforest.client.particle.*;
 import twilightforest.client.properties.*;
 import twilightforest.client.renderer.TFRenderPipelines;
@@ -75,7 +86,6 @@ public final class TFClient implements ClientModInitializer {
 	public void onInitializeClient() {
 		TFTintSources.init();
 
-		TFClientEvents.init();
 		TFSkyRenderer.init();
 		MapDataManager.init();
 		MultipartRenderDispatcher.init();
@@ -102,6 +112,71 @@ public final class TFClient implements ClientModInitializer {
 		registerMapDecorationTypes();
 		registerRenderLayers();
 		registerArmorRenderers();
+
+		setupFogEvents();
+		setupLockedBiomeToastEvents();
+		setupCloudEvents();
+		setupOverlayEvents();
+		setupTravellersClientEvents();
+		setupClientGameEvents();
+	}
+
+	private static void setupFogEvents() {
+		ClientEvents.CARMINITE_COMPUTE_FOG_COLOR.register(FogEventListeners::colorFog);
+	}
+
+	private static void setupLockedBiomeToastEvents() {
+		ClientTickEvents.END_CLIENT_TICK.register(_ -> LockedBiomeToastEventListeners.tickLockedToastLogic());
+	}
+
+	private static void setupCloudEvents() {
+		ClientTickEvents.END_CLIENT_TICK.register(CloudEventListeners::tickWeatherEffects);
+		LevelRenderEvents.AFTER_TRANSLUCENT_TERRAIN.register(CloudEventListeners::renderPrecipitation);
+	}
+
+	private static void setupOverlayEvents() {
+		HudElementRegistry.attachElementAfter(VanillaHudElements.CROSSHAIR, OverlayEventListeners.QUEST_RAM_INDICATOR, (graphics, _) -> OverlayEventListeners.renderIndicator(graphics, graphics.guiWidth(), graphics.guiHeight()));
+		HudElementRegistry.attachElementAfter(VanillaHudElements.MOUNT_HEALTH, OverlayEventListeners.HOSTILE_MOUNT_HUNGER_BAR, (graphics, _) -> OverlayEventListeners.renderHostileMountHungerBar(graphics));
+		HudStatusBarHeightRegistry.addRight(OverlayEventListeners.HOSTILE_MOUNT_HUNGER_BAR, _ -> 10);
+		HudElementRegistry.addLast(OverlayEventListeners.ORE_METER_STATS, (graphics, _) -> OverlayEventListeners.renderOreMeterStats(graphics));
+		HudElementRegistry.attachElementAfter(VanillaHudElements.ARMOR_BAR, OverlayEventListeners.FORTIFICATION_SHIELD_COUNT, (graphics, _) -> OverlayEventListeners.renderShieldCount(graphics, graphics.guiWidth(), graphics.guiHeight()));
+		HudStatusBarHeightRegistry.addLeft(OverlayEventListeners.FORTIFICATION_SHIELD_COUNT, _ -> 10);
+		HudElementRegistry.addLast(OverlayEventListeners.PORTAL_OVERLAY, (graphics, _) -> PortalOverlay.render(graphics));
+		HudElementRegistry.addLast(OverlayEventListeners.ITEM_DISPLAY_OVERLAY, (graphics, _) -> ItemDisplayOverlay.render(graphics, OverlayEventListeners.getCameraPlayer()));
+	}
+
+	private static void setupTravellersClientEvents() {
+		ClientEvents.INPUT_KEY.register(TravellersClientEventListeners::handleDoubleJump);
+		ClientEvents.MOVEMENT_INPUT_UPDATE.register(TravellersClientEventListeners::handleAgileRanger);
+		ClientEvents.MOVEMENT_INPUT_UPDATE.register(TravellersClientEventListeners::handleStraightAhead);
+		ClientEvents.MOVEMENT_INPUT_UPDATE.register(TravellersClientEventListeners::speedUpControlledWhileSneaking);
+		ClientEvents.MOVEMENT_INPUT_UPDATE.register(TravellersClientEventListeners::handleSidestep);
+		ClientEvents.RENDER_FRAME_PRE.register(TravellersClientEventListeners::handleStealth);
+		ClientEvents.COMPUTE_FOV_MODIFIER.register(TravellersClientEventListeners::updateZoomState);
+		ClientEvents.RENDER_FRAME_PRE.register(TravellersClientEventListeners::updateGradualGlideState);
+		ClientEvents.INPUT_KEY.register(TravellersClientEventListeners::cycleItemDisplayMap);
+		ClientEvents.CALCULATE_PLAYER_TURN.register(TravellersClientEventListeners::slowZoomSensitivity);
+		ClientEvents.INPUT_KEY.register(TravellersClientEventListeners::swapHotbar);
+		ClientEvents.INPUT_KEY.register(TravellersClientEventListeners::toggleRedThreadVision);
+	}
+
+	private static void setupClientGameEvents() {
+		ItemTooltipCallback.EVENT.register((stack, _, _, lines) -> ClientGameEventListeners.addCustomTooltips(stack, lines));
+		ClientTickEvents.END_CLIENT_TICK.register(ClientGameEventListeners::clientTick);
+		ScreenEvents.AFTER_INIT.register((_, screen, _, _) -> {
+			ClientGameEventListeners.customizeSplashes(screen);
+			ScreenEvents.remove(screen).register(_ -> ClientGameEventListeners.clearEntityRenderUtilMap());
+		});
+		ClientEvents.RENDER_FRAME_POST.register(ClientGameEventListeners::endAuroraFrame);
+		ClientEvents.RENDER_FRAME_PRE.register(ClientGameEventListeners::killVignette);
+		HudElementRegistry.replaceElement(VanillaHudElements.MOUNT_HEALTH, hudElement -> (graphics, deltaTracker) -> ClientGameEventListeners.removeHostileMountHealth(hudElement, graphics, deltaTracker));
+		ClientEvents.CARMINITE_RENDER_LEVEL_AFTER_WEATHER.register(ClientGameEventListeners::renderAurora);
+		ClientEvents.CUSTOMIZE_BOSS_HEALTH_OVERLAY.register(ClientGameEventListeners::renderCustomBossbars);
+		LevelRenderEvents.BEFORE_BLOCK_OUTLINE.register(ClientGameEventListeners::renderGiantBlockOutlines);
+		ClientEvents.SELECT_MUSIC.register(ClientGameEventListeners::setMusicInDimension);
+		ClientEvents.COMPUTE_CAMERA_ANGLES.register(ClientGameEventListeners::shakeCamera);
+		ItemTooltipCallback.EVENT.register((stack, _, _, lines) -> ClientGameEventListeners.translateBookAuthor(stack, lines));
+		ClientEvents.COMPUTE_FOV_MODIFIER.register(ClientGameEventListeners::updateBowFOV);
 	}
 
 	private static void registerPackets() {
