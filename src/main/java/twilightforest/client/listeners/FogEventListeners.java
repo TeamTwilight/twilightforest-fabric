13c95fc34c8604b364927a07533ff23dc18ec618
Autumn <shinigami7x@proton.me>
2026-09-17 22:27:43 -0400

    Brought over altered version of 71d5df7.

--- src/main/java/twilightforest/client/listeners/FogEventListeners.java
+++ src/main/java/twilightforest/client/listeners/FogEventListeners.java
@@ -4,7 +4,6 @@ import carminite.events.modified.CarminiteComputeFogColorEvent;
 import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.util.Mth;
-import org.jspecify.annotations.Nullable;
 import twilightforest.init.TFBiomes;
 import twilightforest.init.TFDimension;
 
@@ -14,15 +13,14 @@ public final class FogEventListeners {
 	public static void colorFog(CarminiteComputeFogColorEvent event) {
 		if (event.getCamera().entity() instanceof LocalPlayer player && player.level() instanceof ClientLevel client && client.dimension() == TFDimension.DIMENSION_KEY) {
 			float[] colors = new float[]{event.getRed(), event.getGreen(), event.getBlue()};
-			boolean spooky = isSpooky(client, player);
 
 			double time = 13000;
-			double d0 = Mth.frac(time / (double)24000.0F - (double)0.25F);
-			double d1 = (double)0.5F - Math.cos(d0 * Math.PI) / (double)2.0F;
-			double d2 = (float)(d0 * (double)2.0F + d1) / 3.0F;
+			double d0 = Mth.frac(time / (double) 24000.0F - (double) 0.25F);
+			double d1 = (double) 0.5F - Math.cos(d0 * Math.PI) / (double) 2.0F;
+			double d2 = (float) (d0 * (double) 2.0F + d1) / 3.0F;
 			float daylight = Mth.clamp(Mth.cos(d2 * (float) (Math.PI * 2)) * 2.0F + 0.5F, 0.0F, 1.0F);
 
-			if (spooky) {
+			if (client.getBiome(player.blockPosition()).is(TFBiomes.SPOOKY_FOREST)) {
 				spookyPercent += 0.005F;
 			} else {
 				spookyPercent -= 0.005F;
@@ -33,9 +31,5 @@ public final class FogEventListeners {
 			event.setGreen(Mth.clampedLerp(spookyPercent, colors[1] * daylight * 0.94F + 0.06F, colors[1]));
 			event.setBlue(Mth.clampedLerp(spookyPercent, colors[2] * daylight * 0.91F + 0.09F, colors[2]));
 		}
-	}
-
-	private static boolean isSpooky(@Nullable ClientLevel level, @Nullable LocalPlayer player) {
-		return level != null && player != null && level.getBiome(player.blockPosition()).is(TFBiomes.SPOOKY_FOREST);
 	}
 }
