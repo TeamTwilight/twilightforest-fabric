ee84379a139bab19b918183c17fc625f64629638
Autumn <shinigami7x@proton.me>
2026-09-20 15:59:56 -0400

    Brought over altered version of 82c9abf.

--- src/datagen/java/twilightforest/datagen/data/recipes/CraftingGenerator.java
+++ src/datagen/java/twilightforest/datagen/data/recipes/CraftingGenerator.java
@@ -182,7 +182,7 @@ public class CraftingGenerator extends CraftingDataHelper {
 		ShapelessRecipeBuilder.shapeless(getter, RecipeCategory.FOOD, TFItems.MOSS_SOUP)
 			.requires(TFBlocks.MOSS_PATCH)
 			.requires(Items.BOWL)
-			.requires(new ComponentsIngredient(Ingredient.of(Items.POTION), DataComponentPatch.builder().set(DataComponents.POTION_CONTENTS, new PotionContents(Potions.WATER)).build()).toVanilla())
+			.requires(this.potionIngredient(Potions.WATER))
 			.unlockedBy("has_moss", this.has(TFBlocks.MOSS_PATCH))
 			.save(this.output);
 
